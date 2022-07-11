9e77b6ce1968cf4145bddad48ff977efe883480c
Md Rahil <mrahil1139@gmail.com>
2022-07-11 11:20:47 +0530

    adding all files

---
+++ diff.java
@@ -0,0 +1,10 @@
+public class diff
+{  
+    public static void main(String args[])
+    { 
+        int a=5;
+        int b=6;
+     System.out.println("Hello diff file");
+     System.out.println(a+b);
+    }  
+}  
